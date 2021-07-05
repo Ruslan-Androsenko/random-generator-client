00676a1ae8e684647dabac1090e00c6802afe42a
Ruslan.Androsenko <ruslan.androsenko@leomax.ru>
2021-07-05 19:58:24 +0300

    Added configuration for connecting to external api ver. 1.6

--- config/params.php
+++ config/params.php
@@ -5,5 +5,8 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
 
-    'apiMacUrl' => 'http://generator-server.loc/v1', // Хост для доступа к API
+    'apiMacUrl' => 'http://api.mac-generator.ml/v1',    // Хост для доступа к API
+
+    // 'apiMacUrl' => 'http://locahhost:8000/v1',       // Локальный хост для доступа к API
+    // 'apiMacUrl' => 'http://generator-server.loc/v1', // Локальный хост для доступа к API
 ];
